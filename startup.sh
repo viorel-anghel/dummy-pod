26de0c2663adabd0098135d4453890ec46c6793f
Viorel Anghel <viorel.anghel@esolutions.ro>
2025-04-22 15:21:14 +0300

    changing default port to 38080

--- startup.sh
+++ startup.sh
@@ -3,7 +3,7 @@
 t=${TEXT:-ok}
 echo $t >/var/www/html/index.html
 
-p=${PORT:-8080}
+p=${PORT:-38080}
 echo "starting and listening on port $p"
 
 exec /usr/sbin/mini_httpd -p $p -D -d /var/www/html -c "cgi-bin/*" -u www-data
